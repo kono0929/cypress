d25755d58738c4871c2c49a1985df265a6a941b5
writerity-kaori <97166765+writerity-kaori@users.noreply.github.com>
2022-12-19 15:22:35 +0100

    create first test

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-    },
-      specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-      excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*']
+    setupNodeEvents(on, config) {},
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+    excludeSpecPattern: ["**/1-getting-started/*", "**/2-advanced-examples/*"],
   },
 });
---
+++ cypress/e2e/firstTest.spec.js
@@ -0,0 +1,5 @@
+describe("our first test", () => {
+  it("first test", () => {});
+  // by tag name
+  cy.get("");
+});
