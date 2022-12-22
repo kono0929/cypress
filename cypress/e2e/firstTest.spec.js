116d4bcabc2418c774fb44255e8f62339c22e5a3
kono0929 <onokao@kikyos.co.jp>
2022-12-22 13:58:29 +0100

    file Test change

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,38 +1,38 @@
 describe("our first test", () => {
   it("first test", () => {
-    // cy.visit("/");
-    // cy.contains("Forms").click();
-    // cy.contains("Form Layouts").click();
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
 
-    // // by tag name
-    // cy.get("input");
+    // by tag name
+    cy.get("input");
 
-    // //by ID
-    // cy.get("#inputEmail1");
+    //by ID
+    cy.get("#inputEmail1");
 
-    // //by Class name
-    // cy.get(".input-full-width");
+    //by Class name
+    cy.get(".input-full-width");
 
-    // //by Attribute name
-    // cy.get("[placeholder]");
+    //by Attribute name
+    cy.get("[placeholder]");
 
-    // //by Attribute name and value
-    // cy.get('[placeholder="Email"]');
+    //by Attribute name and value
+    cy.get('[placeholder="Email"]');
 
-    // //by Class value
-    // cy.get('[class="input-full-width size-medium shape-rectangle"]');
+    //by Class value
+    cy.get('[class="input-full-width size-medium shape-rectangle"]');
 
-    // //by Tag name and Attribute with value
-    // cy.get('input[placeholder="Email"]');
+    //by Tag name and Attribute with value
+    cy.get('input[placeholder="Email"]');
 
-    // //by two different attributes
-    // cy.get('[placeholder="Email"][fullwidth]');
+    //by two different attributes
+    cy.get('[placeholder="Email"][fullwidth]');
 
-    // //by tag name, Attribute with value, ID and Class name
-    // cy.get('input[placeholder="Email"]#inputEmail1.input-full-width');
+    //by tag name, Attribute with value, ID and Class name
+    cy.get('input[placeholder="Email"]#inputEmail1.input-full-width');
 
-    // //The most recommended way by Cypress
-    // cy.get('[data-cy="imputEmail1"]');
+    //The most recommended way by Cypress
+    cy.get('[data-cy="imputEmail1"]');
   });
 
   it("second test", () => {
@@ -54,5 +54,19 @@ describe("our first test", () => {
         .parents('form')
         .find('nb-checkbox')
         .click()
+
+      cy.contains('nb-card', 'Horizontal form').find('[type="email"]')
+
   });
+
+    it.only('then and wrap methods', () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+ 
+
+  });
+
 });
