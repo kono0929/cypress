215130837932d6b021dd4d84ad5051532fa638e4
kono0929 <onokao@kikyos.co.jp>
2022-12-27 20:31:55 +0100

    invole

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -59,14 +59,86 @@ describe("our first test", () => {
 
   });
 
-    it.only('then and wrap methods', () => {
+    it('then and wrap methods', () => {
+    cy.visit("/")
+    cy.contains("Forms").click()
+    cy.contains("Form Layouts").click()
+
+    // cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+    // cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+    // cy.contains('nb-card', 'Basic form').find('[for="exampleInputEmail1"]').should('contain', 'Email address')
+    // cy.contains('nb-card', 'Basic form').find('[for="exampleInputPassword1"]').should('contain', 'Password')
+
+    //selenium
+    // const firstForm = cy.contains('nb-card', 'Using the Grid')
+    // const secondForm = cy.contains('nb-card', 'Basic form')
+
+    // firstForm.find('[for="inputEmail1"]').should('contain', 'Email')
+    // firstForm.find('[for="inputPassword2"]').should('contain', 'Password')
+    // secondForm.find('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+    //cypress style
+      cy.contains('nb-card', 'Using the Grid').then(firstForm => {
+        const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
+        const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+        expect(emailLabelFirst).to.equal('Email')
+        expect(passwordLabelFirst).to.equal('Password')
+
+        cy.contains('nb-card', 'Basic form').then(secondForm => {
+          const passwordSecondText = secondForm.find('[for="exampleInputPassword1"]').text()
+          expect(passwordLabelFirst).to.equal(passwordSecondText)
+          // when you want to use cypress commands like should, type, click on an object or jquery element you might want to wrap it first
+          // before using the cypress commands.
+          // Simply to say, convert non-cypress or non-cypress workable elements or objects into cypress workable things.
+          cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain', 'Password')
+        })
+
+      })
+
+  });
+
+  it('invoke command', () => {
+
     cy.visit("/");
     cy.contains("Forms").click();
     cy.contains("Form Layouts").click();
+    
+    //1
+    cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
 
-    cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
- 
+    //2
+    cy.get('[for="exampleInputEmail1"]').then( label => {
+      expect(label.text()).to.equal('Email address')
+    })
 
-  });
+    //3
+    cy.get('[for="exampleInputEmail1"]').invoke('text').then( text => {
+      expect(text).to.equal('Email address')
+    })
+
+    cy.contains('nb-card', 'Basic form')
+      .find('nb-checkbox')
+      .click()
+      .find('.custom-checkbox')
+      .invoke('attr', 'class') // .attr() method
+      //.should('contain', 'checked')
+      .then(classValue => {
+        expect(classValue).to.contain('checked')
+      })
+
+  })
+
+  it.only('assert property', () => {
+    cy.visit("/")
+    cy.contains("Forms").click()
+    cy.contains("Datepicker").click()
+
+    cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
+      cy.wrap(input).click()
+      cy.get('nb-calendar-day-cell ').contains('17').click()
+      cy.wrap(input).invoke('prop', 'value').should('contain', 'Dec 17, 2022')
+
+    })
+  })
 
 });
