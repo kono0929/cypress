85f1eb04de97f2646b538df224d66673aa34335f
kono0929 <onokao@kikyos.co.jp>
2022-12-29 15:30:03 +0100

    section 3 is done

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,4 +1,5 @@
 describe("our first test", () => {
+
   it("first test", () => {
     cy.visit("/");
     cy.contains("Forms").click();
@@ -59,7 +60,7 @@ describe("our first test", () => {
 
   });
 
-    it('then and wrap methods', () => {
+  it('then and wrap methods', () => {
     cy.visit("/")
     cy.contains("Forms").click()
     cy.contains("Form Layouts").click()
@@ -97,7 +98,7 @@ describe("our first test", () => {
 
   });
 
-  it('invoke command', () => {
+  it.only('invoke command', () => {
 
     cy.visit("/");
     cy.contains("Forms").click();
@@ -109,6 +110,8 @@ describe("our first test", () => {
     //2
     cy.get('[for="exampleInputEmail1"]').then( label => {
       expect(label.text()).to.equal('Email address')
+      expect(label).to.have.class('label')
+      expect(label).to.have.text('Email address')
     })
 
     //3
@@ -128,17 +131,189 @@ describe("our first test", () => {
 
   })
 
-  it.only('assert property', () => {
+  it('assert property', () => {
     cy.visit("/")
     cy.contains("Forms").click()
     cy.contains("Datepicker").click()
+    
+    function selectDayFromCurrent(day){
+      let date = new Date()
+      date.setDate(date.getDate() + day)
+      let futureDate = date.getDate()
+      let futureMonth = date.toLocaleDateString('en-US', {month: 'short'})
+      let dateAssert = futureMonth + ' ' + futureDate + ', ' + date.getFullYear()
+      console.log(dateAssert)
+      cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then( dateAttribute => {
+        if(!dateAttribute.includes(futureMonth)){
+          cy.get('[data-name="chevron-right"]').click()
+          //cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]').contains(new RegExp("^" + futureDate + "$")).click()
+          selectDayFromCurrent(day)
 
+        }else{
+          cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]').contains(new RegExp("^" + futureDate + "$")).click()
+        }
+      })
+      return dateAssert
+    }
+    
     cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
       cy.wrap(input).click()
-      cy.get('nb-calendar-day-cell ').contains('17').click()
-      cy.wrap(input).invoke('prop', 'value').should('contain', 'Dec 17, 2022')
+      let dateAssert =  selectDayFromCurrent(300)
+      // cy.get('nb-calendar-day-cell ').contains('17').click()
+      cy.wrap(input).invoke('prop', 'value').should('contain', dateAssert)
 
     })
   })
+
+  it('radio button', () => {
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+    cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then( radioButtons => {
+      cy.wrap(radioButtons)
+        .first()
+        .check({force: true})
+        .should('be.checked')
+
+      cy.wrap(radioButtons)
+      // Get A DOM element at a specific index in an array of elements.
+        .eq(1)
+        .check({force: true})
+      
+      cy.wrap(radioButtons)
+        //.first()
+        .eq(0)
+        .should('not.be.checked')
+      
+      cy.wrap(radioButtons)
+        .eq(2)
+        .should('be.disabled')
+
+    })
+  })
+
+  it('check boxes', () => {
+    cy.visit('/')
+    cy.contains('Modal & Overlays').click()
+    //This element must be an <input> with type checkbox or radio.
+    cy.contains('Toastr').click()
+
+    //Check all checkboxes
+    //cy.get('[type="checkbox"]').check({force: true})
+    cy.get('[type="checkbox"]').eq(0).click({force: true})
+    cy.get('[type="checkbox"]').eq(1).check({force: true})
+
+  })
+
+  it('lists and dropdowns', () => {
+    cy.visit('/')
+    //1
+    // cy.get('nav nb-select').click()
+    // cy.get('.options-list').contains('Dark').click()
+    // cy.get('nav nb-select').should('contain', 'Dark')
+    // cy.get('nb-layout-header nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')
+
+    //2
+    cy.get('nav nb-select').then( dropdown => {
+      cy.wrap(dropdown).click()
+      cy.get('.options-list nb-option').each( (listItem, index) => {
+        const itemText = listItem.text().trim()
+
+        const colors = {
+          "Light": "rgb(255, 255, 255)",
+          "Dark": "rgb(34, 43, 69)",
+          "Cosmic": "rgb(50, 50, 89)",
+          "Corporate": "rgb(255, 255, 255)"
+        }
+        
+        cy.wrap(listItem).click()
+        cy.wrap(dropdown).should('contain', itemText)
+        cy.get('nb-layout-header nav').should('have.css', 'background-color', colors[itemText])
+        if( index < 3){
+          cy.wrap(dropdown).click()
+        }
+
+      })
+    })
+  })
+
+  it('Web tables', () => {
+    cy.visit('/')
+    cy.contains('Tables & Data').click()
+    cy.contains('Smart Table').click()
+
+    //1
+    cy.get('tbody').contains('tr', 'Larry').then( tableRow => {
+      cy.wrap(tableRow).find('.nb-edit').click()
+      cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25')
+      cy.wrap(tableRow).find('.nb-checkmark').click()
+      cy.wrap(tableRow).find('td').eq(6).should('contain', '25')
+    })
+
+    //2
+    cy.get('thead').find('.nb-plus').click()
+    cy.get('thead').find('tr').eq(2).then(tableRow => {
+      cy.wrap(tableRow).find('[placeholder="First Name"]').type('Artem')
+      cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Bondar')
+      cy.wrap(tableRow).find('.nb-checkmark').click()
+    })
+
+    cy.get('tbody tr').first().find('td').then( tableColumns => {
+      cy.wrap(tableColumns).eq(2).should('contain', 'Artem')
+      cy.wrap(tableColumns).eq(3).should('contain', 'Bondar')
+    })
+
+    //3
+    const age = [20, 30, 40, 200]
+
+    cy.wrap(age).each( age => {
+        cy.get('thead [placeholder="Age"]').click().clear().type(age)
+        cy.wait(500)
+        cy.get('tbody tr').each( tableRow => {
+          if(age == 200){
+            cy.wrap(tableRow).should('contain', 'No data found')
+          }else{
+            cy.wrap(tableRow).find('td').eq(6).should('contain', age)
+          }
+      })
+    })
+
+  })
+
+  it('tooltip', () => {
+    cy.visit("/");
+    cy.contains("Modal & Overlays").click();
+    cy.contains("Tooltip").click();
+
+    cy.contains('nb-card', 'Colored Tooltips')
+    cy.contains('Default').click()
+    cy.get('nb-tooltip').should('contain', 'This is a tooltip')
+  })
+
+  it('dialog box', () => {
+    cy.visit("/");
+    cy.contains('Tables & Data').click()
+    cy.contains('Smart Table').click()
+
+    //1
+    cy.get('tbody tr').first().find('.nb-trash').click()
+    cy.on('window:confirm', (confirm) => {
+      expect(confirm).to.equal('Are you sure you want to delete?')
+    })
+
+    //2
+    const stub = cy.stub()
+    cy.on('window:confirm', stub)
+    cy.get('tbody tr').first().find('.nb-trash').click().then(() => {
+      expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?')
+    })
+
+    //3 when you want to cancel
+    cy.get('tbody tr').first().find('.nb-trash').click()
+    cy.on('window:confirm', () => false)
+
+  })
+
 
 });
