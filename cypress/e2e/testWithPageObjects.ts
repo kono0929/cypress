ce069ae9467ef86d30538f6bd7c41f0443f92cbd
kono0929 <onokao@kikyos.co.jp>
2023-01-10 15:21:45 +0100

    lesson 47

--- cypress/e2e/testWithPageObjects.ts
+++ cypress/e2e/testWithPageObjects.ts
@@ -1,32 +1,34 @@
-import { navigateTo } from "../support/page_objects/navigationPage"
-import { onFormLayoutPage } from "../support/page_objects/formLayoutPage"
-import { onDatepickerPage } from "../support/page_objects/datepickerPage"
-import { onSmartTablePage } from "../support/page_objects/smartTablePage"
+import { navigateTo } from "../support/page_objects/navigationPage";
+import { onFormLayoutPage } from "../support/page_objects/formLayoutPage";
+import { onDatepickerPage } from "../support/page_objects/datepickerPage";
+import { onSmartTablePage } from "../support/page_objects/smartTablePage";
 
-describe('Test with Page Objects', () => {
+describe("Test with Page Objects", () => {
+  beforeEach("open application", () => {
+    cy.openHomePage();
+  });
+  it("verify navigation across the pages", () => {
+    navigateTo.formLayoutPage();
+    navigateTo.datepickerPage();
+    navigateTo.smartTablePage();
+    navigateTo.tooltipPage();
+    navigateTo.toasterPage();
+  });
 
-    beforeEach('open application', () => {
-        cy.openHomePage()
-    })
-    it('verify navigation across the pages', () =>{
-        navigateTo.formLayoutPage()
-        navigateTo.datepickerPage()
-        navigateTo.smartTablePage()
-        navigateTo.tooltipPage()
-        navigateTo.toasterPage()
-    })
-
-    it.only('should submit Inline and Basic form and select tomorrow date in the calendar', () => {
-        //navigateTo.formLayoutPage()
-        //onFormLayoutPage.submitInlineFormWithNameAndEmail('Kaori', 'kaori@test.com')
-        //onFormLayoutPage.submitBasicFormWithNameAndEmail('kaori@test.com', 'password')
-        // navigateTo.datepickerPage()
-        // onDatepickerPage.selectCommonDatepickerDateFromToday(1)
-        // onDatepickerPage.selectDatepickerWithRangeFromToday(7, 14)
-        navigateTo.smartTablePage()
-        onSmartTablePage.addNewRecordWithFirstAndLastName('Artem', 'Bondar')
-        onSmartTablePage.updateAgeByFirstName('Artem', 25)
-        onSmartTablePage.deleteRowByIndex(1)
-    })
-
-})
+  it.only(
+    "should submit Inline and Basic form and select tomorrow date in the calendar",
+    { browser: "chrome" },
+    () => {
+      //navigateTo.formLayoutPage()
+      //onFormLayoutPage.submitInlineFormWithNameAndEmail('Kaori', 'kaori@test.com')
+      //onFormLayoutPage.submitBasicFormWithNameAndEmail('kaori@test.com', 'password')
+      // navigateTo.datepickerPage()
+      // onDatepickerPage.selectCommonDatepickerDateFromToday(1)
+      // onDatepickerPage.selectDatepickerWithRangeFromToday(7, 14)
+      navigateTo.smartTablePage();
+      onSmartTablePage.addNewRecordWithFirstAndLastName("Artem", "Bondar");
+      onSmartTablePage.updateAgeByFirstName("Artem", 25);
+      onSmartTablePage.deleteRowByIndex(1);
+    }
+  );
+});
